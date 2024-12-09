be965bf01148969f405f00db18d84661ad081794
youngjaeheo2002 <youngjaeheo2002@gmail.com>
2024-12-08 23:51:36 -0500

    added payload logging in the router

--- router/router/utils.py
+++ router/router/utils.py
@@ -7,3 +7,15 @@ def read_local_json(file_name):
         raise FileNotFoundError(f"File {file_name} not found.")
     with open(file_path, "r") as json_file:
         return json.load(json_file)
+
+def write_payload(payload,file_name):
+
+    path = os.path.join(os.path.dirname(__file__),"local_data",file_name)
+    try:
+        with open(path,"w") as file:
+            json.dump(payload, file, indent = 4)
+
+    except Exception as e:
+        print(f"An error has occured: {e}")
+
+    return
